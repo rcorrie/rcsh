9ab271009f73aa69c3798e9fe2fa160f5875474e
Ricardo Corrie <rcorrie90@gmail.com>
2015-11-15 14:39:12 -0500

    Creates gulp workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,57 @@
 'use strict';
 
 var gulp = require('gulp');
+var clean = require('gulp-clean');
+var react = require('gulp-react');
+var watch = require('gulp-watch');
+var uglify = require('gulp-uglify');
+var runSequence = require('run-sequence');
+var vendor = require('gulp-concat-vendor');
+var ngAnnotate = require('gulp-ng-annotate');
+
+gulp.task('build', function() {
+    runSequence(
+        'clean',
+        'vendor',
+        ['copy-src-ng', 'copy-src-jsx']
+    )
+})
+
+gulp.task('watch', function(cb) {
+    watch('src/**/*', function() {
+        gulp.start('build')
+        .on('end', cb)
+    });
+})
+
+gulp.task('clean', function() {
+    return gulp.src('www')
+    .pipe(clean().on('error', error));
+})
+
+gulp.task('vendor', function() {
+    return gulp.src('vendor/*')
+    .pipe(vendor('vendor.js').on('error', error))
+    .pipe(gulp.dest('www/'));
+});
+
+gulp.task('copy-src-ng', function() {
+    return gulp.src('src/js/**/*.js')
+    .pipe(ngAnnotate().on('error', error))
+    .pipe(gulp.dest('www/js/'));
+});
+
+gulp.task('copy-src-jsx', function() {
+    return gulp.src('src/js/views/**/*.jsx')
+    .pipe(react().on('error', error))
+    .pipe(gulp.dest('www/js/views/'));
+});
 
 gulp.task('default', function () {
-  gulp.src('*.js')
+    return gulp.src('*.js')
     .pipe(gulp.dest('*.js'));
 });
+
+function error( e ) {
+    console.error(e);
+}
